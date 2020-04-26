531ac5959a689555cb737f89c8295a03236de55e
Gagandeep Singh Singh <gsmultani11@gmail.com>
2020-04-26 19:58:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+# name = input('WHat is your name :')
+# print('Helllllooooooo ' + name)
+
+#Fundamental Data Types
+int
+float
+complex
+bool
+str
+list
+tuple
+set
+dict
+
+None
+
+# Above are the fundamental data types. however, we can create a custom datatypes called 'Class'
+#Class --> Custom type
+
+
+#Specialized Data Type--> are not built into python but can be used from packages/ Modules (Extension that you can add-on to the language)
+
+#Example: Int numbers
+# print(2+4)
+# print(2/4)
+# print(type(2*3))
+# print(type(2+3))
+# print(type(2/3))
+
+# print(2//3) # returns int
+# print(2/3) #return float
+# print(2%3) #return remander
+# print(2**3) #return power 3
+
+# Math Functions - Action performed on data
+# print(round(3.9))
+# print(abs(-20))
+
+# operator precedence
+# print((20-3*4)-2**2)
+# order - 
+# ()
+# **
+# */
+# +-
+
+# Binary
+# print(bin(15))
+# print(int('0b1111',2))
+
+# Variables
+  # snake_case - user_id not user id
+  #Start with lower case or underscore
+  #Contains letter,number or underscores
+  #Case sensative
+  #Donnot overwrite keywords
+
+#constants - represented by Capital name 
+# PI = 3.14
+# print(PI)
+
+# fast varible assignments 
+# a,b,c = 1,2,3
+# print(b)
+
+
+
+
+
+
+
