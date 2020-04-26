2d026bad711a6d3523fe2ba9309c1387a3683106
Gagan <gsmultani11@gmail.com>
2020-04-26 20:39:32 +0000

    data type, varibles, math operations

--- main.py
+++ main.py
@@ -64,6 +64,15 @@ None
 # print(b)
 
 
+#Agumented assignment operator
+# some_value = 5 
+# some_value = some_value + 2
+# some_value += 2
+# some_value -= 2 
+# some_value *= 2
+# print(some_value)
+
+
 
 
 
