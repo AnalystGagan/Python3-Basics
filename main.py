e8749cb40c94c34adc70bce06c3ec3345ac850a2
Gagan <gsmultani11@gmail.com>
2020-05-03 18:24:05 +0000

    devloped my first GUI

--- main.py
+++ main.py
@@ -389,3 +389,23 @@ None
 #deciding between for and while loop
 #use while loop when you are not sure for how long you need to iterate. 
 # break, continue and pass on loops
+
+#My first GUI!
+#Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
+picture = [
+  [0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0],
+  [0,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,0]
+]
+
+for row in picture:
+  for pix in row:
+    if (pix == 1):
+      print('*', end = "")
+    else:
+      print(' ', end = "")
+  print('')
+    
